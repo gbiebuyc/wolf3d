cc82b1bd225a589fbed40ae84fee6f600dc95eba
gbiebuyc <gbiebuyc@users.noreply.github.com>
2019-03-06 17:42:07 +0100

    Refix compilation on Linux

--- minilibx_linux/mlx_mouse_move.c
+++ minilibx_linux/mlx_mouse_move.c
@@ -6,9 +6,14 @@
 /*   By: nallani <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 19:19:49 by nallani           #+#    #+#             */
-/*   Updated: 2019/03/05 19:36:03 by gbiebuyc         ###   ########.fr       */
+/*   Updated: 2019/03/06 17:39:05 by gbiebuyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int     mlx_mouse_hide()
+{
+	return (0);
+}
 
 int		mlx_mouse_move(void *po, int poo, int pooooo)
 {
